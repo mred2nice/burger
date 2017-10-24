43fe31fb0af5be6c55e69b438a75bd5edee4252a
Ed Dobias <mred2nice@aol.com>
2017-10-24 10:46:51 -0400

    second set of files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, deveoured, date) VALUES ('bacon', False,CURDATE());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('double', False,CURDATE());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('thinlizzy', False,CURDATE());
